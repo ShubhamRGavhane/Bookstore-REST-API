a5712eebdb7bbace34b35406c0e11adc1698ef1a
ShubhamRGavhane <shubhamgavhanebaap@gmail.com>
2025-07-03 19:14:28 +0530

    finalize the bookstore apis

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
+require('dotenv').config();
 
 app.use(express.json());
 app.use(logger);
-require('dotenv').config();
+
 
 app.use('/auth', require('./routes/auth'));
 app.use('/books', require('./routes/books'));
@@ -16,5 +17,5 @@ app.use((req, res) => res.status(404).json({ message: 'Not Found' }));
 module.exports = app;
 
 if (require.main === module) {
-  app.listen(3000, () => console.log('🚀 Server running on http://localhost:3000'));
+  app.listen(3000, () => console.log('Server running on http://localhost:3000'));
 }
